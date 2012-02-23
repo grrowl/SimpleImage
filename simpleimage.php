36b968ed67b2a7b0805197ef33dc53ed4a502d32
Tom McKenzie <tom@chillidonut.com>
2012-02-23 15:35:34 +1100

    More comments, better code layout, minor fixes

--- simpleimage.php
+++ simpleimage.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /*
 * File: SimpleImage.php
 * Author: Simon Jarvis
@@ -18,114 +18,126 @@
 * GNU General Public License for more details:
 * http://www.gnu.org/licenses/gpl.html
 *
-* Modified by Tom McKenzie 2011 to enhance resizing functionality (resizeToDims)
+* v0.9 - Tom McKenzie 2011 - enhanced resizing functionality (resizeToDims)
+* v1.0 - Tom McKenzie 2012/02/23
+		More comments, better code layout, minor fixes
 */
- 
+
 class SimpleImage {
- 
-   var $image;
-   var $image_type;
- 
-   function load($filename) {
- 
-      $image_info = getimagesize($filename);
-      $this->image_type = $image_info[2];
-      if( $this->image_type == IMAGETYPE_JPEG ) {
- 
-         $this->image = imagecreatefromjpeg($filename);
-      } elseif( $this->image_type == IMAGETYPE_GIF ) {
- 
-         $this->image = imagecreatefromgif($filename);
-      } elseif( $this->image_type == IMAGETYPE_PNG ) {
- 
-         $this->image = imagecreatefrompng($filename);
-      }
-		
+	public $image;
+	public $image_type;
+
+	public function __construct($filename = null) {
+		if ($filename !== null)
+			$this->load($filename);
+	}
+
+	public function load($filename) {
+		$image_info = getimagesize($filename);
+		$this->image_type = $image_info[2];
+
+		if( $this->image_type == IMAGETYPE_JPEG ) {
+			$this->image = imagecreatefromjpeg($filename);
+
+		} elseif( $this->image_type == IMAGETYPE_GIF ) {
+			$this->image = imagecreatefromgif($filename);
+
+		} elseif( $this->image_type == IMAGETYPE_PNG ) {
+			$this->image = imagecreatefrompng($filename);
+		}
+
 		if (!$this->image) {
 			throw new Exception("Invalid image type");
 		}
-   }
-   function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
+	}
+	public function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
 		$result = false;
-	
-      if( $image_type == IMAGETYPE_JPEG ) {
-         $result = imagejpeg($this->image,$filename,$compression);
-      } elseif( $image_type == IMAGETYPE_GIF ) {
- 
-         $result = imagegif($this->image,$filename);
-      } elseif( $image_type == IMAGETYPE_PNG ) {
- 
-         $result = imagepng($this->image,$filename);
-      }
-      if( $permissions != null) {
- 
-         chmod($filename,$permissions);
-      }
+
+		if( $image_type == IMAGETYPE_JPEG ) {
+			$result = imagejpeg($this->image,$filename,$compression);
+
+		} elseif( $image_type == IMAGETYPE_GIF ) {
+			$result = imagegif($this->image,$filename);
 		
+		} elseif( $image_type == IMAGETYPE_PNG ) {
+			$result = imagepng($this->image,$filename);
+		}
+		
+		if( $permissions != null) {
+			chmod($filename,$permissions);
+		}
+
 		if (!$result)
 			throw new Exception("Couldn't save image");
-   }
-   function output($image_type=IMAGETYPE_JPEG) {
+	}
+	public function output($image_type=IMAGETYPE_JPEG) {
 		$result = false;
- 
-      if( $image_type == IMAGETYPE_JPEG ) {
-         $result = imagejpeg($this->image);
-      } elseif( $image_type == IMAGETYPE_GIF ) {
- 
-         $result = imagegif($this->image);
-      } elseif( $image_type == IMAGETYPE_PNG ) {
- 
-         $result = imagepng($this->image);
-      }
-		
+
+		if( $image_type == IMAGETYPE_JPEG ) {
+			$result = imagejpeg($this->image);
+
+		} elseif( $image_type == IMAGETYPE_GIF ) {
+			$result = imagegif($this->image);
+
+		} elseif( $image_type == IMAGETYPE_PNG ) {
+			$result = imagepng($this->image);
+		}
+
 		if (!$result)
 			throw new Exception("Couldn't output image");
-   }
-   function getWidth() {
- 
-      return imagesx($this->image);
-   }
-   function getHeight() {
- 
-      return imagesy($this->image);
-   }
-   function resizeToHeight($height) {
- 
-      $ratio = $height / $this->getHeight();
-      $width = $this->getWidth() * $ratio;
-      $this->resize($width,$height);
-   }
- 
-   function resizeToWidth($width) {
-      $ratio = $width / $this->getWidth();
-      $height = $this->getheight() * $ratio;
-      $this->resize($width,$height);
-   }
- 
-   function scale($scale) {
-      $width = $this->getWidth() * $scale/100;
-      $height = $this->getheight() * $scale/100;
-      $this->resize($width,$height);
-   }
-   
-   function resizeToDims($width, $height) {
+	}
+
+	public function getWidth() {
+		return imagesx($this->image);
+	}
+	public function getHeight() {
+		return imagesy($this->image);
+	}
+
+	// scales image to a maximum height
+	public function resizeToHeight($height) {
+		$ratio = $height / $this->getHeight();
+		$width = $this->getWidth() * $ratio;
+		$this->resize($width,$height);
+	}
+	
+	// scales image to a maximum width
+	public function resizeToWidth($width) {
+		$ratio = $width / $this->getWidth();
+		$height = $this->getheight() * $ratio;
+		$this->resize($width,$height);
+	}
+	
+	// scales image by a percentage (100 = no change)
+	public function scale($scale) {
+		$width = $this->getWidth() * $scale/100;
+		$height = $this->getheight() * $scale/100;
+		$this->resize($width,$height);
+	}
+	
+	// resizes an image without cropping
+	public function resizeToDims($width, $height) {
 		$ratio = $width / $height;
 		$origratio =  $this->getWidth() / $this->getHeight();
-      if ($origratio > $ratio) { // wider than dims
+		
+		if ($origratio > $ratio) { // wider than dims
 			$ratio = $width / $this->getWidth();
 			$height = $this->getheight() * $ratio;
+
 		} else { // taller than dims
 			$ratio = $height / $this->getHeight();
 			$width = $this->getWidth() * $ratio;
 		}
-      $this->resize($width,$height);
-   }
- 
-   function resize($width,$height) {
-      $new_image = imagecreatetruecolor($width, $height);
-      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
-      $this->image = $new_image;
-   }      
- 
+
+		$this->resize($width,$height);
+	}
+
+	// resize to a specified width and height
+	public function resize($width, $height) {
+		$new_image = imagecreatetruecolor($width, $height);
+		imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+		$this->image = $new_image;
+	}
+
 }
 ?>
